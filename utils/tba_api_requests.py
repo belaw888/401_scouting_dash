e84f6db1e7360985d6483d57b6e67bb286931528
Béla Williams <belawilliams.xia@gmail.com>
2023-01-05 01:25:54 -0500

    tba data validation prototype

--- utils/tba_api_requests.py
+++ utils/tba_api_requests.py
@@ -1,53 +1,64 @@
+
 import tbapy
 import json
 import pandas as pd
 
 class tba_api_requests:
     
-    def __init__(self, api_key_file_name):
+    def __init__(self, api_key_file_name) -> None:
         self.tba_api_key = open(api_key_file_name, 'r').read()
         self.tba = tbapy.TBA(self.tba_api_key)
         
-    def tbapy_to_pandas_df(json_data):
-        """
-		Converts the json string returned by tbapy functions into a pandas dataframe 
-
-		Args:
-			json_data (String): String format of json data
-
-		Returns:
-			pandas.DataFrame: all n/a values replaced with "" 
-		"""
+    def tbapy_to_dict(self, json_data):
         s1 = json.dumps(json_data)
         data = json.loads(s1)
-        df = pd.DataFrame(data)
-        df = df.fillna('NO_DATA')
-        return df
+        
+        return data
+    
+    def event_match_keys(self, event_key):
+        return self.tba.event_matches(event_key, keys = True)
         
     def match_aggregate_data(self, match_key):
         
-	    data = self.tbapy_to_pandas_df(self.tba.match(match_key))
+        json_data = self.tba.match(match_key)
+        data = self.tbapy_to_dict(json_data)
+        match_code = data['comp_level'] + str(data['match_number'])
+        
+        df = pd.DataFrame.from_dict(data['score_breakdown'])
+        
+        df = df.fillna('NO_DATA')
+        df = df.transpose()
+        
+        column_list = ['totalPoints', 'matchCargoTotal',
+                       'autoPoints', 'autoCargoPoints', 'autoCargoTotal', 'autoTaxiPoints',
+                       'teleopPoints', 'teleopCargoPoints', 'teleopCargoTotal',
+                       'endgamePoints', 'endgameRobot1', 'endgameRobot2', 'endgameRobot3']
+        
+        df = df[column_list]
+        
+        df['Robot1'] = [int(data['alliances'][x]['team_keys'][0][3:]) for x in df.index]
+        df['Robot2'] = [int(data['alliances'][x]['team_keys'][1][3:]) for x in df.index]
+        df['Robot3'] = [int(data['alliances'][x]['team_keys'][2][3:]) for x in df.index]
+        
+        df['match_number'] = data['match_number']
+        df['comp_level'] = data['comp_level']
+        df['alliance_color'] = df.index
+                
+        df = df.rename(index={
+            'blue' : f'{match_code}_blue',
+            'red': f'{match_code}_red'})
 
-  
-  
-l = tba_api_requests('tba_api_key.txt')
-
-h = l.match_total_score('2022chcmp_qm1')
-
-print(h)
-
-
-     
+        return df
     
-		
-     
-		# self.a=api_key
-  
-    	
-    	# self.
-     
-     
-		# tba = tbapy.TBA(
-    	# 'dZURQZdsSGuLmOC8lHnCnpPvjUqVpQ2qXxdObgcLS75cT7jNAfUxxvkOusgsd30e')
- 
- 
+    def event_aggregate_data(self, event_key):
+    
+        match_keys = self.event_match_keys(event_key)
+        filtered_match_keys = filter(lambda key: 'qm' in key, match_keys)
+        filtered_match_keys = list(filtered_match_keys)
+    
+        df = self.match_aggregate_data(filtered_match_keys[0])
+
+        for match in filtered_match_keys[1:]:
+            df = pd.concat([df, self.match_aggregate_data(match)])
+
+        return df
--- utils/tba_data_validation.py
+++ utils/tba_data_validation.py
@@ -1,4 +1,129 @@
+import pandas as pd
+from tenacity import TryAgain
+import tba_api_requests 
+from pandas.errors import MergeError
+
+import pygsheets
+import pandas as pd
+
+gc = pygsheets.authorize(service_file=
+    '/home/belawilliams/Documents/team-401-scouting-credentials-2023.json')
+
+sh = gc.open('401_scouting_test_database')
+
+#select the first sheet 
+wks = sh[0]
+
+df = wks.get_as_df(
+    has_header=True, 
+    # index_column=5,
+    include_tailing_empty=False,
+    include_tailing_empty_rows=False,
+    value_render='FORMATTED_VALUE')
+
+# print(df)
+# print(df.shape)
 
 class tba_data_validation:
     
-     
+    local_scouting_data = pd.DataFrame()
+    local_tba_data = pd.DataFrame()
+    match_key_queue = []
+    
+    def __init__(self) -> None:
+        self.tbareq = tba_api_requests.tba_api_requests('tba_api_key.txt')
+        
+    def set_local_scouting_data(self, data) -> None:
+        self.local_scouting_data = data
+    
+    def get_DataFrame_differences(self, df1, df2):
+        try:
+            data_combined = df1.merge(df2, indicator = True, how = 'outer')
+            data_combined_diff = data_combined.loc[lambda x : x['_merge'] != 'both']
+            return data_combined_diff
+
+        except MergeError:
+            return 'error' #will slow things down bc returns whole df if there are no updates on refresh
+                
+    def validate_new_data(self, updated_scouting_data):
+        
+        # print(self.local_scouting_data)
+        
+        new_scouting_data = self.get_DataFrame_differences(
+            self.local_scouting_data,
+            updated_scouting_data)
+        
+        self.local_scouting_data = new_scouting_data
+        
+        local = self.local_scouting_data
+        print(local)
+        validation_list = []
+        
+        keys_set = set(local['tba_api_id'].to_list())
+        unique_keys = list(keys_set)
+        
+        for key in unique_keys:
+            validation_list.append(self.validate_one_match(key))
+    
+        # for row in new_scouting_data:
+        #     try:
+        #     	self.tbareq.match_aggregate_data(row['tba_api_id'])
+                         
+        
+        return validation_list
+        
+        
+    # def team_score_contribution()
+    
+    def validate_one_match(self, match_key):
+        
+        local = self.local_scouting_data
+        # print(local['tba_api_id'])
+        # print(local)
+        
+        try:
+            tba_match_data = self.tbareq.match_aggregate_data(match_key)
+            
+            match_filter = (local['tba_api_id'] == match_key)
+            
+            # print(local.loc[match_filter, 'tba_api_id'])
+            # print(local['Robot Position'][0])
+            # print('Blue' in local['Robot Position'][0])
+            # print(local.loc[local['Robot Position'].str.contains('Blue', na=False), 'Robot Position'])
+            
+            red_filter = (
+                match_filter & (local['Robot Position'].str.contains('Red', na=False)))
+            
+            blue_filter = (
+                match_filter & (local['Robot Position'].str.contains('Blue', na=False)))
+            
+            # print(local.loc[red_filter, 'Upper Hub Scored'].sum())
+            
+            scouting_tele_balls = (
+                local.loc[red_filter, 'Upper Hub Scored'].sum() + 
+                local.loc[red_filter, 'Lower Hub Scored'].sum() +
+                local.loc[blue_filter, 'Upper Hub Scored'].sum() +
+                local.loc[blue_filter, 'Lower Hub Scored'].sum())
+            
+            tba_tele_balls = tba_match_data['teleopCargoTotal'].sum()
+            
+            if abs(scouting_tele_balls - tba_tele_balls) > 1:
+                return match_key
+            else:
+                return "Within Tolerance" #change to null
+            
+        
+        except: #find out what error this will be
+            self.match_key_queue.append(match_key)
+            return 'TBA not updated, added to queue'
+        
+        
+val = tba_data_validation()
+val.set_local_scouting_data(df.loc[0:180])
+# print(df.loc[7:5])
+print(val.validate_new_data(df))
+# val.get_DataFrame_differences(df, df.loc[20:])
+
+
+# print(val.validate_one_match('2022varr_qm2'))
+
