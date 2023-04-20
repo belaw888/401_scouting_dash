41c0308e086c8ce5f5e91206e14c8e6197b9f75e
Béla <76421215+belaw888@users.noreply.github.com>
2023-04-20 11:37:17 -0400

    Update match_view.py

--- pages/match_view.py
+++ pages/match_view.py
@@ -25,7 +25,7 @@ sheets_data = sheets.get_google_sheets_dataframe()
 teams_list = sheets.get_team_list()
 
 tba = tbapy.tba_api_requests('tba_api_key.txt')
-event_key = 'gcmp'
+event_key = 'gal'
 
 def update_matches(event_key):
     match_keys = tba.event_match_keys(event_key)
